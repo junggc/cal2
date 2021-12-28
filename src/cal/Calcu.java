90e9b7031feae3ee98f181ab28fb70218b19968d
junggc <jung@kacelab.com>
2021-12-28 15:33:22 +0900

    Initial commit

---
+++ src/cal/Calcu.java
@@ -0,0 +1,27 @@
+package cal;
+
+public class Calcu {
+
+    private int a;
+    private String b;
+    private int c;
+
+    public int getA(){
+        return a;
+    }
+    public void setA(int a){
+        this.a = a;
+    }
+    public String getB(){
+        return b;
+    }
+    public void setB(String b){
+        this.b = b;
+    }
+    public int getC(){
+        return c;
+    }
+    public void setC(int c){
+        this.c = c;
+    }
+}
